fb4ea9c90f7a0308986016b6415a41e645236cad
Zhikina <Swan2489@gmail.com>
2018-10-14 19:08:47 +0700

    Test 2

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -4,14 +4,14 @@ public class MainClassTest extends MainClass
 {
   @Test
 
-  public void testGetLocalNumber()
+  public void testGetClassNumber()
   {
 
-    int number = this.getLocalNumber();
-    if(number == 14) {
-      System.out.println("Sucsess! Function returned value " + number);
+    int number = this.getClassNumber();
+    if(number > 45) {
+      System.out.println("Sucsess! Value " + number +" more than 45");
     }else {
-      System.out.println("Error! Value: "+ number);
+      System.out.println("Error! Value: "+ number + " less than 45");
     }
 
 
